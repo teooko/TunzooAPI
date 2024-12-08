4a960359888d2d6127358aa689e183feac6635b4
teooko <teobogdann@yahoo.com>
2024-12-08 20:22:23 +0200

    Create a redis service

--- TunzooAPI.Services/RedisService.cs
+++ TunzooAPI.Services/RedisService.cs
@@ -1,10 +1,33 @@
+using StackExchange.Redis;
+using TunzooApi.Domain.Entities;
+
 namespace TunzooAPI.Services;
 
 public interface IRedisService
 {
-    
+    Lobby SetLobby();
+    string GetLobby(string lobbyId);
 }
-public class RedisService
+public class RedisService : IRedisService
 {
-    
+    private readonly IConnectionMultiplexer _redis;
+
+    public RedisService(IConnectionMultiplexer redis)
+    {
+        _redis = redis;
+    }
+
+    public Lobby SetLobby()
+    {
+        var db = _redis.GetDatabase();
+        var lobby = new Lobby();
+        db.HashSet($"lobby:{lobby.LobbyId}:metadata", new HashEntry[] {new HashEntry("gameState", "waiting")});
+        return lobby;
+    }
+
+    public string GetLobby(string lobbyId)
+    { 
+        var db = _redis.GetDatabase();
+        return db.HashGet($"lobby:{lobbyId}:metadata", "gameState");
+    }
 }
